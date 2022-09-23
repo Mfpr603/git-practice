fd14a85b277186e9a6012d5dc65183eb95bffc3b
Jan li <bingox29@gmail.com>
2022-09-23 13:53:29 -0400

    Jan completed function b

--- function_b.py
+++ function_b.py
@@ -7,7 +7,28 @@ def silly_sum():
         the user enters 0, or
         the sum reaches or exceeds 1000
     """
-    pass
+    sum = 0
+    while sum < 1000:
+        num = input_int()
+        sum += num
+        if num == 0:
+            break
+    return sum
+
+
+def input_int(*args):
+    # loop forever (until we get valid input)
+    while True:
+        # get string input
+        user_input = input(*args)
+        try:
+            # try converting to an int (will return if successful)
+            return int(user_input)
+
+            # if the conversion succeeded, we will have returned already
+        except ValueError:
+            # could not convert the input, so show an error message
+            print('Try again.')
 
 
 if __name__ == "__main__":
